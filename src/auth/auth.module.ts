f4fe321fbdb13e99b0e71c0d6847a8311aa37424
Mike Ryb <myhou@mail.ru>
2026-03-20 17:00:03 +0300

    JWTModuleAddEnvParams

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,14 +2,21 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import type { StringValue } from "ms";
 
 @Module({
   imports: [
     JwtModule.registerAsync({
       imports: [],
-      useFactory: () => ({
-        secret: 'should be from config',
-      }),
+      useFactory: () => {
+        const secret = process.env.JWT_SECRET;
+        if (!secret) throw new Error('JWT_SECRET is not defined');
+        const expiresIn = process.env.JWT_EXPIRES_IN  as StringValue || '1h';
+        return {
+          secret,
+          signOptions: { expiresIn },
+        };
+      },
       inject: [],
    }),
   ],
