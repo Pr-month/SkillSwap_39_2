1b3ae01aa2d28f99a5c7b8931a0d2e0bfeec1f6d
Nadya Nikolaeva <nadya.nik.ven@yandex.ru>
2026-03-20 11:25:33 +0300

    add jwtConfig -08

---
+++ src/config/jwt.config.ts
@@ -0,0 +1,10 @@
+import { registerAs } from '@nestjs/config';
+
+export const jwtConfig = registerAs('JWT_CONFIG', () => ({
+  access_token_key: process.env.ACCESS_TOKEN_KEY || 'secret-dev',
+  access_token_expiry: process.env.ACCESS_TOKEN_EXPIRY || '10m',
+  refresh_token_key: process.env.REFRESH_TOKEN_KEY || 'secret-dev',
+  refresh_token_expiry: process.env.REFRESH_TOKEN_EXPIRY || '7d',
+}));
+
+export type TJwtConfig = ConfigType<typeof jwtConfig>;
