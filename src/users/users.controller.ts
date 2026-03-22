f9592a8c221a945a0f37c90e34907659537a02e0
Mariam Tetruashvili <maritetruashvili17@gmail.com>
2026-03-22 14:17:19 +0400

    feat: GET users/me endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Req,
+  UseGuards,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JwtAuthGuard } from '../auth/guards/jwtAuth.guard';
+import { RequestWithUser } from '../auth/types/request-with-user.interface';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async getMe(@Req() req: RequestWithUser): Promise<Omit<User, 'password'>> {
+    const user = await this.usersService.findById(req.user.sub);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const { password, ...safeUser } = user;
+
+    return safeUser;
+  }
 }
