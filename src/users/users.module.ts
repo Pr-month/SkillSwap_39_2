f59bdf40eda65153b13193c560e444762ca78c46
Roland <machodin@yandex.ru>
2026-03-24 18:14:16 +0300

    remove dublicated import

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 
 @Module({
