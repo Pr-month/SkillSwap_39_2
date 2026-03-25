3d0ef2cded84060930fe83b89f8ef4ece854fcda
Mariam Tetruashvili <maritetruashvili17@gmail.com>
2026-03-25 14:23:02 +0400

    Feat: hide password and refreshToken using ClassSerializerInterceptor

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { TAppConfig } from './config/app.config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,8 +16,12 @@ async function bootstrap() {
     }),
   );
 
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
   const configService = app.get(ConfigService);
   const appConfigData = configService.get<TAppConfig>('APP_CONFIG');
+
   await app.listen(appConfigData?.port ?? 3000);
 }
+
 void bootstrap();
