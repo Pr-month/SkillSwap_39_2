7bf6c3ed119992de226e374adac553edea8fd79d
Mariam Tetruashvili <maritetruashvili17@gmail.com>
2026-03-25 15:16:38 +0400

    Обновление навыка по эндпоинту PATCH skills/:id

--- src/skills/skills.controller.ts
+++ src/skills/skills.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Patch, Param, Body, UseGuards, Req } from '@nestjs/common';
 import { SkillsService } from './skills.service';
+import { UpdateSkillDto } from './dto/update-skill.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwtAuth.guard';
+import { IRequestWithUser } from '../auth/types/express';
 
 @Controller('skills')
 export class SkillsController {
   constructor(private readonly skillsService: SkillsService) {}
 
-  @Get()
-  findAll() {
-    return "it just works";
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Req() req: IRequestWithUser,
+    @Body() updateSkillDto: UpdateSkillDto,
+  ) {
+    return this.skillsService.update(id, req.user!.id, updateSkillDto);
   }
 }
--- src/skills/skills.service.ts
+++ src/skills/skills.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  ForbiddenException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Skill } from './entities/skill.entity';
+import { UpdateSkillDto } from './dto/update-skill.dto';
 
 @Injectable()
-export class SkillsService {}
+export class SkillsService {
+  constructor(
+    @InjectRepository(Skill)
+    private readonly skillsRepository: Repository<Skill>,
+  ) {}
+
+  async update(id: string, userId: string, updateSkillDto: UpdateSkillDto) {
+    const skill = await this.skillsRepository.findOne({
+      where: { id },
+      relations: ['user'],
+    });
+
+    if (!skill) {
+      throw new NotFoundException('Skill not found');
+    }
+
+    if (skill.userId !== userId) {
+      throw new ForbiddenException('You can update only your own skill');
+    }
+
+    Object.assign(skill, updateSkillDto);
+    return await this.skillsRepository.save(skill);
+  }
+}
