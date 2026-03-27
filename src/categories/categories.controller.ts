b3e8c2c3ef60daaa22bcea77414d5514f5cecb7d
Mariam Tetruashvili <maritetruashvili17@gmail.com>
2026-03-27 14:08:51 +0400

    feat: add GET /categories endpoint

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { Category } from './entities/category.entity';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Get()
+  async getAllCategories(): Promise<Category[]> {
+    return this.categoriesService.findAll();
+  }
+}
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
+  async findAll(): Promise<Category[]> {
+    return this.categoryRepository.find({
+      relations: ['parent', 'children'],
+    });
+  }
+}
