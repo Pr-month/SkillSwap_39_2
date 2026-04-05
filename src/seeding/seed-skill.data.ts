101c2f9eb9e5aeef3b65df65ae01b10ff774cda8
IREN <iren@example.com>
2026-04-05 09:47:29 +0300

    fix: correct skills seeding

--- src/seeding/seed-skill.data.ts
+++ src/seeding/seed-skill.data.ts
@@ -1,3 +1,5 @@
+import { SeedUserData } from './seed-user.data';
+
 export interface SeedCreateSkill {
   title: string;
   description: string;
@@ -13,27 +15,27 @@ export const SeedSkillData: SeedCreateSkill[] = [
     name: 'drums',
     description: 'Привет! Научу базовым ритмам, постановке рук и динамике.',
     categoryName: 'Ударные',
-    email: 'ivan.petrov@example.com',
+    email: SeedUserData[0].email,
   },
   {
     title: 'React для начинающих',
     name: 'react-basic',
     description: 'Компоненты, хуки, роутинг, best practices.',
     categoryName: 'Frontend',
-    email: 'anna.smirnova@example.com',
+    email: SeedUserData[1].email,
   },
   {
     title: 'Английский для путешествий',
     name: 'english-travel',
     description: 'Разговорные шаблоны, аудирование, базовая грамматика и словарь для поездок.',
     categoryName: 'Английский язык',
-    email: 'dmitry.kozlov@example.com',
+    email: SeedUserData[2].email,
   },
   {
     title: 'Маркетинг и реклама: стратегия и креативы',
     name: 'marketing-ads-strategy',
     description: 'Позиционирование, УТП, воронка, тест гипотез и разбор рекламных креативов.',
     categoryName: 'Маркетинг и реклама',
-    email: 'elena.volkova@example.com',
+    email: SeedUserData[3].email,
   },
 ];
