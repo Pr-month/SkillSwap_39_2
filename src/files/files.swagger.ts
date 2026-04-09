39d88cd804a241cfb138c528dc91f04a2396e4b3
usmor <7887330@gmail.com>
2026-04-09 13:43:42 +0300

    feat(files): add API documemtation for files module

---
+++ src/files/files.swagger.ts
@@ -0,0 +1,65 @@
+import { applyDecorators } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
+
+export const ApiUploadFile = () => {
+  return applyDecorators(
+    ApiOperation({ summary: 'Загрузить изображение' }),
+    ApiConsumes('multipart/form-data'),
+    ApiBody({
+      description: 'Файл изображения любого формата (мин. 1 КБ)',
+      schema: {
+        type: 'object',
+        properties: {
+          file: {
+            type: 'string',
+            format: 'binary',
+          },
+        },
+        required: ['file'],
+      },
+    }),
+    ApiResponse({
+      status: 201,
+      description: 'Файл успешно загружен',
+      schema: {
+        type: 'object',
+        properties: {
+          statusCode: { type: 'number', example: 201 },
+          message: { type: 'string', example: 'Файл успешно загружен' },
+          data: {
+            type: 'object',
+            properties: {
+              fileName: {
+                type: 'string',
+                example: '/public/uploads/unique-filename.jpg',
+              },
+              originalName: { type: 'string', example: 'my-photo.jpg' },
+              size: { type: 'number', example: 1048576 },
+              mimeType: { type: 'string', example: 'image/jpeg' },
+            },
+          },
+        },
+      },
+    }),
+    ApiResponse({
+      status: 400,
+      description: 'Ошибка при загрузке файла',
+      schema: {
+        type: 'object',
+        properties: {
+          statusCode: { type: 'number', example: 400 },
+          message: {
+            type: 'string',
+            enum: ['Размер файла слишком мал', 'Ошибка при сохранении файла'],
+          },
+          error: { type: 'string', example: 'Bad Request' },
+        },
+      },
+    }),
+  );
+};
