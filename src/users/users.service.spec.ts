7b3bff35eb2e48464f363ec0d79d8aa6edf1821f
spiritualbless <yaroslav.svakhin@yandex.ru>
2026-04-15 14:38:46 +0300

    fixed test at users.service

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import type { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { appConfig } from '../config/app.config';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let usersRepository: jest.Mocked<Repository<User>>;
 
   beforeEach(async () => {
+    usersRepository = {
+      find: jest.fn(),
+      findOne: jest.fn(),
+      findAndCount: jest.fn(),
+      create: jest.fn(),
+      save: jest.fn(),
+      delete: jest.fn(),
+      update: jest.fn(),
+    } as any;
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: usersRepository,
+        },
+        {
+          provide: appConfig.KEY,
+          useValue: { hashSalt: 10, port: 3000, adminData: {} },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
