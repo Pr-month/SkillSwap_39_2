357ecc15d6f95f384310a8ef25b996b8e6d3b59b
usmor <7887330@gmail.com>
2026-04-15 18:56:28 +0300

    fix(lint): fix linting errors in app.config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,4 @@
 import { ConfigType, registerAs } from '@nestjs/config';
-import passport from 'passport';
 import { Gender } from 'src/users/users.enums';
 
 export const appConfig = registerAs('APP_CONFIG', () => ({
